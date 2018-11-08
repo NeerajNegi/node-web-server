7971061e53338c18c6e4a01e5d1fe1816fd18ea1
neerajnegi174 <neerajnegi@univinks.com>
2018-11-08 15:34:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server logs.');
+		}
+	});
+	next();
+})
+
+// Maintenance Middleware
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		name: 'Neeraj Negi'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+	console.log(`Server is running at port: ${port}`);
+});
