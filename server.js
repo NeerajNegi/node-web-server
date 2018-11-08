1a633ab05fd1e45e7c0678ad253dbdbd74187264
neerajnegi174 <neerajnegi@univinks.com>
2018-11-08 17:30:11 +0530

    Procfile Removed

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Server is running at port: ${port}`);
